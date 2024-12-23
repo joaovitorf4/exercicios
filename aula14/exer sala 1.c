ca7a829cbbae08a33a846be452cab909f7eba0b4
joaovitorf4 <joaovitorfod5@gmail.com>
2024-12-23 16:58:08 -0300

    aula 14

---
+++ aula14/exer sala 1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int A, *B, **C, ***D;
+    
+    scanf("%d", &A);
+    
+    B = &A;
+    C = &B;
+    D = &C;
+    
+    printf("Dobro: %d\n", *B * 2);
+    printf("Triplo: %d\n", **C * 3);
+    printf("Quadruplo: %d\n", ***D * 4);
+
+    return 0;
+}
