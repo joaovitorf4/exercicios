aa33edfab0cf2d105f134c0944f20d63ce4200bf
joaovitorf4 <joaovitorfod5@gmail.com>
2025-01-14 16:17:44 -0300

    accepted

--- lista_bee9/1769.c
+++ lista_bee9/1769.c
@@ -4,28 +4,27 @@
 int isValidCPF(char cpf[]) {
     int digits[11];
 
-    for ( int i = 0; i < 11; i++ ) {
-        if ( cpf[i] >= '0' && cpf[i] <= '9' ) {
-            digits[i] = cpf[i] - '0';
+    for (int i = 0, j = 0; i < 14; i++) {
+        if (cpf[i] >= '0' && cpf[i] <= '9') {
+            digits[j++] = cpf[i] - '0';
         }
     }
 
     int sum1 = 0;
-    for ( int i = 0; i < 9; i++ ) {
-        sum1 += digits[i] * ( i + 1 );
+    for (int i = 0; i < 9; i++) {
+        sum1 += digits[i] * (i + 1);
     }
-
     int b1 = sum1 % 11;
-    if ( b1 == 10 ) b1 = 0;
+    if (b1 == 10) b1 = 0;
 
     int sum2 = 0;
-    for ( int i = 0; i < 9; i++ ) {
-        sum2 += digits[i] * ( 9 - i );
+    for (int i = 0; i < 9; i++) {
+        sum2 += digits[i] * (9 - i);
     }
     int b2 = sum2 % 11;
-    if ( b2 == 10 ) b2 = 0;
+    if (b2 == 10) b2 = 0;
 
-    if ( digits[9] == b1 && digits[10] == b2 ) {
+    if (digits[9] == b1 && digits[10] == b2) {
         return 1;
     } else {
         return 0;
@@ -35,8 +34,8 @@ int isValidCPF(char cpf[]) {
 int main() {
     char cpf[15];
 
-    while ( scanf("%s", cpf) != EOF ) {
-        if ( isValidCPF(cpf) ) {
+    while (scanf("%s", cpf) != EOF) {
+        if (isValidCPF(cpf)) {
             printf("CPF valido\n");
         } else {
             printf("CPF invalido\n");
